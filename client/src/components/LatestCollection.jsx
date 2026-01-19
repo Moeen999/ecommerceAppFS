276c73b4d1eb1148a554d5616e40e824185a1c44
Muhammad Moeen <mooenmalik374@gmail.com>
2026-01-19 20:19:33 +0500

    Added the latest collection section to display the latest product items also added the dynamic route for a specific product item to be displayed.

--- client/src/components/LatestCollection.jsx
+++ client/src/components/LatestCollection.jsx
@@ -1,13 +1,42 @@
-import { useContext } from "react"
-import { ShopContext } from "../context/ShopContext"
+import { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
 
 const LatestCollection = () => {
-    const {products} = useContext(ShopContext);
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { products } = useContext(ShopContext);
+  const [productItem, setProductItem] = useState([]);
 
-export default LatestCollection
+  useEffect(() => {
+    setProductItem(products?.slice(0, 10));
+  }, []);
+
+  return (
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"LATEST"} text2={"COLLECTION"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          No more visits to physical clothing stores. FOREVER brings you a
+          premium online shopping experience with a wide range of high-quality
+          apparel, delivered straight to your doorstep.
+        </p>
+      </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {productItem.map((item) => {
+          console.log(item);
+          return (
+            <ProductItem
+              key={item._id}
+              _id={item._id}
+              image={item.image}
+              name={item.name}
+              price={item.price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default LatestCollection;
