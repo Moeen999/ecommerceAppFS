5fb13c00689ae31d6876630efec7cdf8afa074db
Muhammad Moeen <mooenmalik374@gmail.com>
2026-01-24 00:39:25 +0500

    Added the searchbar in the collection page to search the items.

--- client/src/context/ShopContext.jsx
+++ client/src/context/ShopContext.jsx
@@ -1,19 +1,25 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/assets";
 
 export const ShopContext = createContext();
 
 const ShopContextProvider = ({ children }) => {
+  const [searchVal, setSearchVal] = useState("");
+  const [showSearchBar, setShowSearchBar] = useState(false);
+
   const currency = "$";
   const delivery_fee = 10;
   const value = {
     currency,
     products,
     delivery_fee,
+    searchVal,
+    setSearchVal,
+    showSearchBar,
+    setShowSearchBar,
   };
 
   return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>;
 };
-
 
 export default ShopContextProvider;
--- client/src/pages/Collection.jsx
+++ client/src/pages/Collection.jsx
@@ -72,7 +72,7 @@ const Collection = () => {
   }, [category, subCategory]);
 
   return (
-    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
+    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 ">
       {/* FILTERS */}
       <div className="min-w-60">
         <p
