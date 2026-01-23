2304be0c46bd6301d2954057365d495967f83244
Muhammad Moeen <mooenmalik374@gmail.com>
2026-01-24 00:51:00 +0500

    Implemented the logic to show the searchbar only on the collection page for better UX.

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,11 +1,23 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/assets";
+import { useLocation } from "react-router-dom";
 
 const SearchBar = () => {
   const { searchVal, setSearchVal, showSearchBar, setShowSearchBar } =
     useContext(ShopContext);
-  return showSearchBar ? (
+  const [visible, setVisible] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    if (pathname.includes("collection")) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  }, [pathname]);
+
+  return visible && showSearchBar ? (
     <div className="bg-gray-50 text-center">
       <div className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2 ">
         <input
@@ -18,7 +30,7 @@ const SearchBar = () => {
         <img className="w-4" src={assets.search_icon} alt="" />
       </div>
       <img
-      onClick={()=>setShowSearchBar(false)}
+        onClick={() => setShowSearchBar(false)}
         className="inline w-3 cursor-pointer"
         src={assets.cross_icon}
         alt=""
