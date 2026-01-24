2c2926eaa6692864cf94705076683b65cc1a21f0
Muhammad Moeen <mooenmalik374@gmail.com>
2026-01-24 16:40:53 +0500

    Added the related products to the selected product in the dynamic product page.

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import { useParams } from "react-router-dom";
 import { assets } from "../assets/assets";
+import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
   const { productId } = useParams();
@@ -112,6 +113,12 @@ const Product = () => {
           </p>
         </div>
       </div>
+
+      {/* RELATED PRODUCTS */}
+      <RelatedProducts
+        category={productData.category}
+        subCategory={productData.subCategory}
+      />
     </div>
   ) : (
     <div className="opacity-0"></div>
