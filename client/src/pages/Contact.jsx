55260011d9860ea0e98498ac4646f67a5ff77c20
Muhammad Moeen <mooenmalik374@gmail.com>
2026-01-26 12:26:24 +0500

    Added the content in the contact page also added the styling for it along with the responsive design, Also along with it the Frontend is finalized.

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,10 +1,42 @@
+import Title from "../components/Title";
+import { assets } from "../assets/assets";
+import NewsLetterBox from "../components/NewsLetterBox"
 
 const Contact = () => {
   return (
     <div>
-      
-    </div>
-  )
-}
+      <div className="text-center text-2xl pt-10 border-t">
+        <Title text1={"CONTACT"} text2={"US"} />
+      </div>
 
-export default Contact
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img
+          src={assets.contact_img}
+          alt=""
+          className="w-full md:max-w-[480px]"
+        />
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-semibold text-xl text-gray-600">Our Store</p>
+          <p className="text-gray-500">
+            54709 Willms Station <br /> Suite 350, Washington, USA
+          </p>
+          <p className="text-gray-500">
+            Tel: (+123) 555-01234 <br /> Email: admin@forever.com
+          </p>
+          <p className="font-semibold text-xl text-gray-600">
+            Careers at FOREVER
+          </p>
+          <p className="font-semibold text-xl text-gray-600">
+            Learn moe about our teams and job openings
+          </p>
+          <button className="border border-black px-8 py-4 text-sm hover:text-white hover:bg-black transition-all duration-500">
+            Explore Jobs
+          </button>
+        </div>
+      </div>
+      <NewsLetterBox/>
+    </div>
+  );
+};
+
+export default Contact;
