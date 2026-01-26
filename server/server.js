e1fbe5f08c1b14f0c2fa8cf739639cae723a2973
Muhammad Moeen <mooenmalik374@gmail.com>
2026-01-26 22:51:19 +0500

    Configured the cloudinary on the server to store the images on the cloud service provider as cloudinary.

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/db.js";
+import connectCloudinary from "./config/cloudinary.js";
 
 // configured App
 const app = express();
 const port = process.env.PORT || 4000;
-connectDB()
+connectDB();
+connectCloudinary();
 
 // middlewares
 app.use(express.json());
