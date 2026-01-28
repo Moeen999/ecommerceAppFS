1459d78cd73f9d39f533c3498d124037e465a98b
Muhammad Moeen <mooenmalik374@gmail.com>
2026-01-28 18:34:35 +0500

    Implemented the logic in the add product controller and also added the multer middleware to add the product and store its data into the DB

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,4 +1,56 @@
-const addProduct = async (req, res) => {};
+import { v2 as cloudinary } from "cloudinary";
+import productModel from "../models/productModel.js";
+
+const addProduct = async (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      price,
+      category,
+      subCategory,
+      sizes,
+      bestseller,
+    } = req.body;
+    const image1 = req.files.image1 && req.files.image1[0];
+    const image2 = req.files.image2 && req.files.image2[0];
+    const image3 = req.files.image3 && req.files.image3[0];
+    const image4 = req.files.image4 && req.files.image4[0];
+
+    const images = [image1, image2, image3, image4].filter(
+      (item) => item !== undefined,
+    );
+
+    let imagesURL = await Promise.all(
+      images.map(async (img) => {
+        let res = await cloudinary.uploader.upload(img.path, {
+          resource_type: "image",
+        });
+        return res.secure_url;
+      }),
+    );
+
+    const productData = {
+      name,
+      description,
+      category,
+      price: Number(price),
+      subCategory,
+      bestseller: bestseller === "true" ? true : false,
+      sizes: JSON.parse(sizes),
+      image: imagesURL,
+      date: Date.now(),
+    };
+
+    const product = await productModel(productData);
+    await product.save();
+
+    res.json({ success: true, message: "Product Added." });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 const removeProduct = async (req, res) => {};
 
