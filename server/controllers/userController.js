bcdb99cff5b6ddcb95c1657fad2b29b1fc079765
Muhammad Moeen <mooenmalik374@gmail.com>
2026-01-28 15:14:17 +0500

    Implemented the logic in the user register controller to create an account of the user.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,62 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import validator from "validator"
+
 // ! Resgitser user controller
 
-const registerUser = async (req, res) => {};
+import userModel from "../models/userModel.js";
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const userExists = await userModel.findOne({ email });
+
+    if (userExists) {
+      return res.json({
+        success: false,
+        message: "User with this email already exists, try a different one.",
+      });
+    }
+
+    if (validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Please enter a valid email address.",
+      });
+    }
+
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Please enter a strong password.",
+      });
+    }
+
+    const saltValue = await bcrypt.genSalt(10);
+
+    const hashedPassword = await bcrypt.hash(password, saltValue);
+
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+
+    const token = generateToken(user._id);
+
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // ! login user controller
 
