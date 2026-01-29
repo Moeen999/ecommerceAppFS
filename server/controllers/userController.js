a86d454fc07245f3c91b06f4885582a53b29926d
Muhammad Moeen <mooenmalik374@gmail.com>
2026-01-29 12:31:54 +0500

    Implemented the logic in the admin login controller function so that admin can login with his credentials.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -84,6 +84,18 @@ const loginUser = async (req, res) => {
 
 // ! Admin login controller
 
-const adminLogin = async (req, res) => {};
+const adminLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+      const token = jwt.sign(email + password, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid Credentials!" });
+    }
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
 
 export { registerUser, loginUser, adminLogin };
