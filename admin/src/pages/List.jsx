217e2992138f2a306b58d510698c47f95408b33a
Muhammad Moeen <mooenmalik374@gmail.com>
2026-01-30 23:02:33 +0500

    Added the logic to make the API call in a function to fetch the products data from the DB.

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { serverUrl } from "../App";
 
 const List = () => {
-  return (
-    <div>
-      list
-    </div>
-  )
-}
+  const [products, setProuducts] = useState([]);
 
-export default List
+  const fetchProducts = async () => {
+    try {
+      const res = await axios.get(serverUrl + "/api/product/list");
+      setProuducts(res.data.products);
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default List;
