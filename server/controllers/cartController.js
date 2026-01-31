037e28fca38b2b0f8c174b3fe398b859ecfc9f78
Muhammad Moeen <mooenmalik374@gmail.com>
2026-01-31 22:30:23 +0500

    Implemnted the logic in the add to cart controller function to allow the user to add a product and save its data in the DB at real time.

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -1,7 +1,30 @@
-const addToCart = async (req, res) => {};
+import userModel from "../models/userModel";
 
-const getCartData = async (req, res) => {};
+const addToCart = async (req, res) => {
+  try {
+    const { userId, itemId, size } = req.body;
+    const user = await userModel.findById(userId);
+    let cartData = await user.cartData;
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+    await userModel.findByIdAndUpdate(userId, { cartData });
+    res.json({ success: true, message: "Added to cart!" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 const updateCart = async (req, res) => {};
+
+const getCartData = async (req, res) => {};
 
 export { addToCart, getCartData, updateCart };
